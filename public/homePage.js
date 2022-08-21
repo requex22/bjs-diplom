fcc04390c38c4bdfb3b36372957a00c23203a2cb
Stanislav <stas_sokolov_2001@mail.ru>
2022-08-21 16:12:37 +0600

    finaly version in diplom project

--- public/homePage.js
+++ public/homePage.js
@@ -2,13 +2,15 @@
 
 const logoutButton = new LogoutButton();
 const ratesBoard = new RatesBoard();
+const moneyManager = new MoneyManager();
+const favoritesWidget = new FavoritesWidget();
 
 logoutButton.action = () => {
-	ApiConnector.logout((response) => {
+	ApiConnector.logout(response => {
 		if (response.succes) {
 			location.reload();
 		}
-	})
+	});
 };
 
 ApiConnector.current(response => {
@@ -17,14 +19,81 @@ ApiConnector.current(response => {
 	}
 });
 
-const getRates = () => {
-	ApiConnector.getStocks(response => {
-		if (response.succes) {
-			FavoritesWidget.clearTable();
-			FavoritesWidget.fillTable(response.data);
-		} 
-	})
+function getRates() {
+    ApiConnector.getStocks(response => {
+        if (response.success){
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data)
+        }
+    });
 };
 
 getRates();
+setInterval(getRates, 60 * 1000);
 
+moneyManager.addMoneyCallback = (data => {
+	ApiConnector.addMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(true, 'Пополнение средств произошло успешно');
+		} else {
+			moneyManager.setMessage(false, response.error);
+		}
+	});	
+});
+
+moneyManager.conversionMoneyCallback = (data => {
+	ApiConnector.convertMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(true, 'Конвертация валюты произошла успешно');
+		} else {
+			moneyManager.setMessage(false, response.error);
+		}
+	});
+});
+
+moneyManager.sendMoneyCallback = (data => {
+	ApiConnector.transferMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(true, 'Перевод средств пользователю произошёл успешно');
+		} else {
+			moneyManager.setMessage(false, response.error);
+		}
+	});
+});
+
+ApiConnector.getFavorites(response => {
+	if (response.success) {
+		favoritesWidget.clearTable();
+		favoritesWidget.fillTable(response.data);
+		MoneyManager.updateUsersList(response.data);
+	}
+})
+
+favoritesWidget.addUserCallback = (id => {
+	ApiConnector.addUserToFavorites(id, response => {
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			MoneyManager.updateUsersList(response.data);
+			favoritesWidget.setMessage(true, 'Пользователь добавлен');
+		} else {
+			favoritesWidget.setMessage(false, response.error);
+		}
+	});
+});
+
+favoritesWidget.removeUserCallback = (id => {
+	ApiConnector.removeUserFromFavorites(id, response => {
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			MoneyManager.updateUsersList(response.data);
+			favoritesWidget.setMessage(true, 'Пользователь удалён');
+		} else {
+			favoritesWidget.setMessage(false, response.error);
+		}
+	});
+});
